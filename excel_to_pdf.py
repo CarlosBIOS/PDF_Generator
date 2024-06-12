dee1118e147e0416791d95a3f59aa24250cabfa0
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-12 19:48:49 +0100

    Initial commit

--- excel_to_pdf.py
+++ excel_to_pdf.py
@@ -1,1 +1,17 @@
-import fpdf
+from fpdf import FPDF
+import pandas as pd
+import glob
+
+filepaths = glob.glob('Invoices/*.xlsx')
+# ['Invoices\\10001-2023.1.18.xlsx', 'Invoices\\10002-2023.1.18.xlsx', 'Invoices\\10003-2023.1.18.xlsx']
+
+for filepath in filepaths:
+    data = pd.read_excel(filepath, sheet_name='Sheet 1')  # Só dá quando instalar openpyxl!!!
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w=50, h=8, txt=f'Invoice nr. {filepath.strip('Invoices\\.xlsx')[:5]}', align='L', ln=1, border=0)
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w=50, h=8, txt=f'Date {filepath.strip('Invoices\\.xlsx')[6:]}', align='L', ln=1, border=0)
+
+    pdf.output(f'PDFs/{filepath[9:].replace('xlsx', 'pdf')}')
---
+++ txt_to_pdf.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF
+import glob
+
+filepaths = glob.glob('TextFiles/*.txt')
+
+for filepath in filepaths:
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=24)
+    title = filepath.replace('TextFiles\\', '').replace('.txt', '').title()
+    pdf.cell(w=50, h=10, txt=f'{title}', align='L', border=0)
+    pdf.ln()
+    pdf.set_font(family='Times', size=12)
+    with open(filepath, encoding='utf-8') as file:
+        for line in file:
+            pdf.multi_cell(w=180, h=8, txt=f'{line.strip()}')
+
+    pdf.output(f'TextFiles/{filepath[9:].replace('txt', 'pdf')}')
