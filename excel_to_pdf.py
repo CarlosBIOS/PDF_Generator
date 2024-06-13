2eea2f2123e27d7786a203701dfda00b944a1da4
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-13 21:24:20 +0100

    add Header, table, total price, image

--- excel_to_pdf.py
+++ excel_to_pdf.py
@@ -10,8 +10,40 @@ for filepath in filepaths:
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=50, h=8, txt=f'Invoice nr. {filepath.strip('Invoices\\.xlsx')[:5]}', align='L', ln=1, border=0)
+    pdf.cell(w=50, h=8, txt=f'Invoice nr. {filepath.strip('Invoices\\.xlsx')[:5]}', align='L', ln=1)
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=50, h=8, txt=f'Date {filepath.strip('Invoices\\.xlsx')[6:]}', align='L', ln=1, border=0)
+    pdf.cell(w=50, h=8, txt=f'Date {filepath.strip('Invoices\\.xlsx')[6:]}', align='L', ln=1)
+
+    all_columns: list = [item.replace('_', ' ').title() for item in list(data.columns)]
+    pdf.set_font(family='Times', size=12, style='B')
+    pdf.ln()  # Criar parágrafo
+    pdf.cell(w=30, h=8, txt=str(all_columns[0]), border=1)
+    pdf.cell(w=60, h=8, txt=str(all_columns[1]), border=1)
+    pdf.cell(w=40, h=8, txt=str(all_columns[2]), border=1)
+    pdf.cell(w=33, h=8, txt=str(all_columns[3]), border=1)
+    pdf.cell(w=30, h=8, txt=str(all_columns[4]), border=1, ln=1)
+    for index, row in data.iterrows():
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=33, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    pdf.set_font(family='Times', size=12)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=60, h=8, txt='', border=1)
+    pdf.cell(w=40, h=8, txt='', border=1)
+    pdf.cell(w=33, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt=str(data['total_price'].sum()), border=1, ln=1)
+
+    pdf.ln()
+
+    pdf.set_font(family='Times', size=15, style='B')
+    pdf.cell(w=30, h=8, txt=f'The total price is {data['total_price'].sum()}', ln=1)
+
+    pdf.set_font(family='Times', size=15, style='B')
+    pdf.cell(w=30, h=8, txt='PythonHow')
+    pdf.image('pythonhow.png', w=10)
 
     pdf.output(f'PDFs/{filepath[9:].replace('xlsx', 'pdf')}')
